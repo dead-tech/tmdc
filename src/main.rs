19bbc01e9cff9207df5e9a9ec0ea0f32bf983fa4
Luca Natilla <dev.dead.tech@gmail.com>
2021-07-03 18:59:24 +0200

    Multiline code blocks support newlines, improved way of parsing code blocks and changed the parsing on each line of the file.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use core::panic;
-use regex::{Regex, RegexBuilder};
+use regex::Regex;
 use std::collections::VecDeque;
 use std::env::args;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read, Seek, SeekFrom, Write};
-use std::slice;
+use std::io::{BufRead, BufReader, Write};
 
 fn parse_token(mut token: VecDeque<char>, mut line: String) -> String {
-    println!("Token: {:#?}\n Line: {:#?}", &token, &line);
+    // println!("Token: {:#?}\n Line: {:#?}", &token, &line);
 
     if line == "---" {
         let s = format!("<hr />");
@@ -39,19 +38,7 @@ fn parse_token(mut token: VecDeque<char>, mut line: String) -> String {
             //TODO: There's only one know problem for this implementation:
             // - newlines aren't kept so the text in the code tag will always appear on the same line.
             // This happens because i replace \n with " ", should be pretty easy to fix but tedious.
-            if line.matches("`").count() == 6 {
-                // println!("Actual Line: {:?}", line);
-                let multiline_code_regex = RegexBuilder::new(r"[`]{3}\n(?P<w>.+)\n[`]{3}")
-                    .dot_matches_new_line(true)
-                    .build()
-                    .unwrap();
-
-                line = multiline_code_regex
-                    .replace_all(&line, "<code>\n$w</code>\n")
-                    .to_string();
-
-                return line;
-            } else if line.matches("`").count() == 2 {
+            if line.matches("`").count() == 2 {
                 // println!("Line: {:?} => {:?}", line, "Single");
                 let single_line_code_regex = Regex::new(r"[`]{1}(?P<w>.+)[`]{1}").unwrap();
                 line = single_line_code_regex
@@ -84,124 +71,74 @@ fn parse_token(mut token: VecDeque<char>, mut line: String) -> String {
     ret
 }
 
-fn new_parse_code_block(reader: &mut BufReader<File>) -> (Vec<String>, Vec<String>) {
-    let mut read_buffer: Vec<u8> = vec![];
-    let mut buffers: Vec<Vec<u8>> = Vec::new();
-
-    let mut code_blocks: Vec<String> = Vec::new();
-    let mut inner_codeblocks: Vec<String> = Vec::new();
-
-    let mut bytes_read: usize = 1;
-
-    // let lines: Vec<_> = reader.lines().map(|el| el.unwrap()).collect();
-
-    // let test: Vec<_> = lines
-    //     .iter()
-    //     .enumerate()
-    //     .filter(|(_, el)| el.starts_with("```"))
-    //     .map(|el| el.0)
-    //     .collect();
-
-    // println!("{:?}", test);
-
-    // let code_blocks: Vec<_> = test
-    //     .chunks(2)
-    //     .map(|el| {
-    //         lines
-    //             .iter()
-    //             .skip(el[0] + 1)
-    //             .take(el[1] - el[0] - 1)
-    //             .collect::<Vec<_>>()
-    //     })
-    //     .collect();
-
-    // println!("{:?}", code_blocks);
-
-    while bytes_read != 0 {
-        read_buffer.clear();
-
-        let mut line = String::new();
-
-        let _ = reader
-            .by_ref()
-            .read_line(&mut line)
-            .unwrap_or(usize::max_value());
-        bytes_read = reader
-            .by_ref()
-            .read_until(b'`', &mut read_buffer)
-            .unwrap_or(usize::max_value());
-
-        if line.contains("``") {
-            buffers.push(read_buffer.clone());
-        }
-    }
-
-    // WARN: This is an hack
-    buffers.pop();
-
-    for buffer in buffers {
-        if buffer.len() > 1 {
-            let mut codeblock = String::from_utf8(buffer.clone()).unwrap();
-            codeblock = codeblock[..&codeblock.len() - 1].to_string();
-            codeblock.insert_str(0, "```\n");
-            codeblock.push_str("\n```");
-
-            code_blocks.push(codeblock)
-        }
-    }
-
-    let inner_code_blocks_cleaned: Vec<_> = code_blocks
-        .clone()
+fn parse_code_block(lines: &Vec<String>) -> VecDeque<Vec<String>> {
+    let code_blocks_indices: Vec<_> = lines
         .iter()
-        .map(|el| el.replace("```", "").replace("\n", ""))
+        .enumerate()
+        .filter(|(_, el)| el.starts_with("```"))
+        .map(|el| el.0)
         .collect();
 
-    for t in inner_code_blocks_cleaned {
-        let split = t.split("\r").filter(|el| *el != "");
+    let code_blocks = code_blocks_indices
+        .chunks(2)
+        .map(|el| {
+            lines
+                .iter()
+                .skip(el[0] + 1)
+                .take(el[1] - el[0] - 1)
+                .map(String::clone)
+                .collect::<Vec<_>>()
+        })
+        .collect();
 
-        for s in split {
-            inner_codeblocks.push(s.to_string());
-        }
-    }
-
-    (code_blocks, inner_codeblocks)
+    code_blocks
 }
 
 fn parse_markdown_file(file_path: &String) {
-    let mut file = File::open(file_path).expect("Could not open file.");
-    let mut reader = BufReader::new(file.try_clone().unwrap());
+    let file = File::open(file_path).expect("Could not open file.");
+    let input_lines: Vec<_> = BufReader::new(file.try_clone().unwrap())
+        .lines()
+        .map(Result::unwrap)
+        .collect();
 
-    let mut lines: Vec<String> = Vec::new();
+    let mut output_lines: Vec<String> = Vec::new();
+    let mut code_blocks = parse_code_block(&input_lines);
 
-    let mut codeblock_tags_counter = 0;
-    let mut current_codeblock = 0;
-    let (codeblocks, inner_codeblocks) = new_parse_code_block(reader.by_ref());
+    let mut it = input_lines.iter();
 
-    let _ = file.seek(SeekFrom::Start(0));
+    // enum Token {
+    //     CodeBlock(Vec<String>),
+    //     List(Vec<String>),
+    // }
 
-    for line in reader.by_ref().lines() {
-        let mut line: String = match line {
-            Ok(contents) => contents,
-            Err(err) => panic!("Invalid token: {}", err.to_string()),
-        };
+    // let test = Token::code_block(vec![]);
+
+    // match test {
+    //     Token::code_block(content) => {}
+    // }
+
+    while let Some(line) = it.next() {
         let token: VecDeque<char> = line.chars().take_while(|ch| *ch != ' ').collect();
 
-        if line.matches("`").count() == 3 && codeblock_tags_counter % 2 == 0 {
-            line = codeblocks[current_codeblock].clone();
-            codeblock_tags_counter += 1;
-            current_codeblock += 1;
-        } else if line.matches("`").count() == 3 {
-            codeblock_tags_counter += 1;
-        }
+        if line.starts_with("```") {
+            if code_blocks.len() > 0 {
+                let block = code_blocks.pop_front().unwrap();
 
-        if token.len() > 0 && !inner_codeblocks.iter().any(|el| *el == line) {
-            lines.push(parse_token(token, line));
+                it.nth(block.len());
+
+                output_lines.push(format!(
+                    "<code>\n<pre>\n{}</pre>\n</code>\n",
+                    block.join("\n")
+                ));
+            }
+        } else if token.len() > 0 {
+            output_lines.push(parse_token(token, line.clone()));
         }
     }
 
     let mut outfile = File::create("test.html").expect("Could not create output file");
 
-    for line in &lines {
+    for line in &output_lines {
         outfile
             .write_all(line.as_bytes())
             .expect("Could not write to output file.")
